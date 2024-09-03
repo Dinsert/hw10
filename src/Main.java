11979940ec096634a49126342c485363f9c69ee9
Наталья <korolkov.01@bk.ru>
2024-09-03 18:23:58 +0300

    homework10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+//        Task 1
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+//        Task 2
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+//        Task 3
+        String fullName1 = "Иванов Семён Семёнович";
+        String fullName2 = fullName1.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName2);
     }
 }
